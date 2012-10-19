9f866835b727dbaf62528b536bdfe1e9a6541c85
aheadley <aheadley@nexcess.net>
2012-10-19 12:48:28 -0400

    oops, copy+paste bit me

--- ee-cli-installer.php
+++ ee-cli-installer.php
@@ -1,7 +1,7 @@
 <?php
 
 /**
- * Nexcess.net Wordpress CLI Installer
+ * Nexcess.net ExpressionEngine CLI Installer
  * Copyright (C) 2011  Nexcess.net L.L.C.
  *
  * This program is free software; you can redistribute it and/or
